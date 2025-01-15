14fd03d086e0880604299debd1db87665d1ea237
jirokk <kakaijiro@gmail.com>
2025-01-15 12:21:35 +0900

    Adds country list feature and flag emoji conversion
    Introduces a new CountryList component to display a list of countries
    Implements flag emoji to PNG conversion for better flag display
    Updates CityItem and CountryItem components to use the new flag conversion
    Adds Barcelona to the cities data
    
    Fixes #123

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -6,7 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://127.0.0.1:9000";
 
@@ -28,7 +30,7 @@ function App() {
       }
     }
     fetchData();
-    console.log(cities);
+    // console.log(cities);
   }, []);
 
   return (
@@ -49,7 +51,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,15 @@
 /* eslint-disable react/prop-types */
 import styles from "./CityItem.module.css";
+// import { flagemojiToPNG } from "./util/helper";
+
+const flagemojiToPNG = (flag) => {
+  const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -13,7 +23,7 @@ function CityItem({ city }) {
 
   return (
     <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
+      <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>{formatDate(date)}</time>
       <button className={styles.deleteBtn}>&times;</button>
